24fc5bdd7ff029ee5eab7451c43beee743ce8be6
jhting <jhting@test.com>
2021-11-29 22:06:58 +0800

    1：添加main文件

---
+++ app/appmain/lib/include/git_log_version_number.h
@@ -0,0 +1,13 @@
+/* This is auto make file! by build_cmd.sh/cmd_get_git_version_number*/
+
+#ifndef __GIT_VERSION_NUMBER_H__
+#define __GIT_VERSION_NUMBER_H__
+
+#define GIT_COMMIT 	"3790e956c3d917a416b69fad77e6b46a13ef50fe"  
+
+#define GIT_LOG_DATE 		"Mon Nov 29 21:55:13 2021 +0800"  
+
+#define GIT_COMMIT_AUTH 	"jhting"  
+
+#endif
+
